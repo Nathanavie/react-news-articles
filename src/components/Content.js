61caa2d8a102ab518b98448c83f2539d2ef3812b
Nathan Atherton <nathanatherton2@gmail.com>
2019-12-05 17:16:55 +0000

    Added link to relevant news source on each story

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,12 +3,32 @@ import Title from './Title'
 // import Author from './Author'
 import Img from './Img'
 import Desc from './Desc'
+import Source from './Source'
 
 class Content extends React.Component {
 
   openArticle = event => {
     console.log('clicked on article link')
 
+  }
+
+  openSource = e => {
+    let sourceLink = this.props.source.name
+    if (sourceLink.includes("BBC")) {
+      window.open("https://www.bbc.co.uk/news")
+    }
+    if (sourceLink.includes("Sky.com")) {
+      window.open("https://news.sky.com")
+    }
+    if (sourceLink.includes(".co.uk")) {
+      window.open("https://" + sourceLink)
+    }
+    if (sourceLink.includes(".com")) {
+      window.open("https://" + sourceLink)
+    }
+    if (sourceLink.includes("Independent")) {
+      window.open("https://www.independent.co.uk")
+    }
   }
 
 
@@ -20,17 +40,22 @@ class Content extends React.Component {
       )
     }
     return(
-      <a className="articleLink" href={article.url} target="_blank">
-        <div className="newsArticle">
-          <div className="articleImage">
-          <Img img={article.urlToImage} link={article.url}/>
+      <div className="articleLink">
+        <a href={article.url} target="_blank">
+          <div className="newsArticle">
+            <div className="articleImage">
+            <Img img={article.urlToImage} link={article.url}/>
+            </div>
+            <div className="articleWording">
+              <Title title={article.title}/>
+              <Desc desc={article.description} />
+            </div>
           </div>
-          <div className="articleWording">
-            <Title title={article.title}/>
-            <Desc desc={article.description} />
-          </div>
+        </a>
+        <div onClick={this.openSource} className="source">
+        <Source source={article.source.name}/>
         </div>
-      </a>
+      </div>
     )
   }
 }
