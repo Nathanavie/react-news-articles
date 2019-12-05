caecd352ffda983819bf6c6d095f98bb69e37cd1
Nathan Atherton <nathanatherton2@gmail.com>
2019-12-05 09:59:37 +0000

    Added build file
    > Netlify hosting wont work without this in the repo

---
+++ build/precache-manifest.91a412b3dd0b02a83ea78101579e4f65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f186a3c11b66809d4d0216c496973f71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d6730614c82b9c60af4",
+    "url": "/static/css/main.414c4036.chunk.css"
+  },
+  {
+    "revision": "5f1b8187efa69b08c18c",
+    "url": "/static/js/2.f3388a26.chunk.js"
+  },
+  {
+    "revision": "9d6730614c82b9c60af4",
+    "url": "/static/js/main.e14a6030.chunk.js"
+  },
+  {
+    "revision": "85baf30b2635db6daf95",
+    "url": "/static/js/runtime-main.77bac873.js"
+  }
+]);
